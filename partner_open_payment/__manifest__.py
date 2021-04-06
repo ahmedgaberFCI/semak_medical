4e7b5d4f31ff39723eb3285ab4e69b56fe7f4fbb
ezzatsamy <ezzatsamy@semak-eg.com>
2021-04-06 16:14:12 +0300

    partner open payment

---
+++ partner_open_payment/__manifest__.py
@@ -0,0 +1,23 @@
+
+################################################################################
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+################################################################################
+{
+    'name': "Invoice Tree",
+    'summary': """Invoice Tree""",
+    'description': """Invoice Tree.""",
+    'author': "Ahmed Gaber",
+    'depends': ['account'],
+    'data': [
+
+            'reports/partner_open_report.xml',
+
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
