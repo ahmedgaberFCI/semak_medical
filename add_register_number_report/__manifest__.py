d262ee0d68511e081e418b6cd3d2c95363a1f312
Ahmed Gaber <ahmedgaber2050@yhoo.com>
2021-05-30 15:16:21 +0300

    add_register_number_report

---
+++ add_register_number_report/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Add Register Number To  Report",
+    'summary': """Add Register Number To  Report""",
+    'description': """Add Register Number To  Report""",
+    'author': "Ahmed Gaber",
+    'license': 'AGPL-3',
+    'category': 'account',
+    'version': '13.0',
+    'depends': ['base','web'],
+    'data': [
+        'views/layout.xml',
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
