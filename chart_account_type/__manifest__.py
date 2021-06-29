3405bf3c7ee6c087adf3ffd2a732ba864141ddff
Ahmed Gaber <ahmedgaber2050@yhoo.com>
2021-06-29 12:21:16 +0300

    chart account type menu

---
+++ chart_account_type/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': "Chart Of Account Type",
+    'summary': """Add menu for Chart Of Account Type""",
+    'description': """Add menu for Chart Of Account Type""",
+    'author': "Ahmed Gaber",
+    'license': 'AGPL-3',
+    'category': 'account',
+    'version': '14.0',
+    'depends': ['base','account'],
+    'data': [
+        'views/account_type_view.xml',
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': True,
+}
